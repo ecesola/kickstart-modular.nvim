7c87f82e076fa3c688db6cc563d049a80dab6613
ecesola <pablo.chorradas@gmail.com>
2024-04-21 13:29:33 +0200

    Adding extra keymaps

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -34,6 +34,9 @@ vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right win
 vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
 vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
 
+vim.keymap.set('v', '>', '>gv', { desc = 'After tab in re-select the same' })
+vim.keymap.set('v', '<', '<gv', { desc = 'After tab in re-select the same' })
+
 -- [[ Basic Autocommands ]]
 --  See `:help lua-guide-autocommands`
 
